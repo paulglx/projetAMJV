098e7841309b3e64b99a7a87c6c1da784781bc68
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-12-19 12:52:35 +0100

    Update default solution and add level object in WizardScene

--- Assets/Scripts/Capacité perso/CapacityWizard.cs
+++ Assets/Scripts/Capacité perso/CapacityWizard.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CapacityWizard : Capacity
 {
 
     [SerializeField] private GameObject wall;
+    [SerializeField] private GameObject level;
 
     public override bool Use(GameObject target, Vector3 point)
     {
@@ -13,7 +16,14 @@ public class CapacityWizard : Capacity
         if (target)
             return false;
 
-        Instantiate(wall, point, Quaternion.identity);
+        // If there is no target, create a wall
+        // The wall should be offset by half its size to be centered on the point
+        point += new Vector3(0, wall.transform.localScale.y / 2, 0);
+
+        Instantiate(wall, point, Quaternion.identity, level.transform);
+
+        FindObjectOfType<NavMeshSurface>().BuildNavMesh();
+
         return true;
     }
 }
