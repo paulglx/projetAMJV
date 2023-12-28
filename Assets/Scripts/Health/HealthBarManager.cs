c2089ffeae9bd910099ad823e67e105d0fea609a
paulglx <paul.guilloux@telecom-sudparis.eu>
2023-12-28 10:52:19 +0100

    Show HealthBar only when damaged

--- Assets/Scripts/Health/HealthBarManager.cs
+++ Assets/Scripts/Health/HealthBarManager.cs
@@ -7,15 +7,17 @@ public class HealthBarManager : MonoBehaviour
 {
 
     [SerializeField] private GameObject healthBarFull;
-    private Image healthBarFullImage;
-    private Health parentHealth;
+    private GameObject healthBarCanvas;
     private float targetFillAmount;
+    private Health parentHealth;
+    private Image healthBarFullImage;
 
     // Start is called before the first frame update
     void Start()
     {
         parentHealth = GetComponentInParent<Health>();
         healthBarFullImage = healthBarFull.GetComponent<Image>();
+        healthBarCanvas = GetComponentInChildren<Canvas>().gameObject;
 
         targetFillAmount = parentHealth.GetHealth() / parentHealth.GetMaxHealth();
     }
@@ -24,6 +26,7 @@ public class HealthBarManager : MonoBehaviour
     void Update()
     {
         healthBarFullImage.fillAmount = Mathf.Lerp(healthBarFullImage.fillAmount, targetFillAmount, 0.5f * Time.deltaTime * 60);
+        healthBarCanvas.SetActive(healthBarFullImage.fillAmount < 1);
     }
 
     public void UpdateHealthbar()
