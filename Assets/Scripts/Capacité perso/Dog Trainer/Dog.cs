59c28005b8d3f793beadaccb9520031878cea206
paulglx <paul.guilloux@telecom-sudparis.eu>
2024-01-05 14:32:02 +0100

    if trainer dies, dogs turn in rounds

--- Assets/Scripts/Capacité perso/Dog Trainer/Dog.cs
+++ Assets/Scripts/Capacité perso/Dog Trainer/Dog.cs
@@ -8,6 +8,7 @@ public class Dog : MonoBehaviour
     [SerializeField] private GameObject dogTrainer;
     private Attack attack;
     private MovementManager movementManager;
+    [SerializeField] private bool isLost;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +20,30 @@ public class Dog : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (isLost)
+        {
+            movementManager.GoToPoint(transform.position + transform.forward + transform.right);
+            return;
+        }
+
         if (attack.GetTarget() == null)
         {
             movementManager.GoToPoint(dogTrainer.transform.position);
+        }
+
+        if (!isLost)
+        {
+            if (dogTrainer == null)
+            {
+                isLost = true;
+            }
         }
     }
 
     public void SetDogTrainer(GameObject dogTrainer)
     {
         this.dogTrainer = dogTrainer;
+        isLost = false;
     }
 }
