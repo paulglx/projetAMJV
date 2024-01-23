1fbb5e37ab752016d4efe435ed904e62c96b95fd
Yanare7 <rayanedahdah1@gmail.com>
2024-01-23 10:51:19 +0100

    (ysrer

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
 
+    private Time temps; 
+    private int numberOfEnemies;
+
+    private GameObject king; 
+
+    
 
 
 
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
         SubscribeToFlag();
+
     }
 
     // Update is called once per frame
@@ -23,8 +29,28 @@ public class Game : MonoBehaviour
     }
 
 
+    private void SetTheKing(GameObject player)
+    {
+        king = player;
+    }
+
     private void SubscribeToFlag()
     {
+        flag = GameObject.Find("Flag");
 
+        if (flag != null)
+        {
+            flag.GetComponent<FlagManager>().FlagCaptured.AddListener(SetTheKing);
+        }
+        else
+        {
+            Debug.LogWarning("The gameObject \"Flag\" is not found in the current scene.");
+        }
+    }
+
+
+    public void OneDie()
+    {
+        numberOfEnemies -=1
     }
 }
