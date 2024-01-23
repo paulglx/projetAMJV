ca2002ea7d5ea9bd1f1679e94e8a361ae4552d2b
Yanare7 <rayanedahdah1@gmail.com>
2024-01-23 11:31:51 +0100

    f,khk

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     private GameObject flag; 
-    private Time temps; 
+    private float temps; 
     private int numberOfEnemies;
-
     private GameObject king; 
-
-    
-
-
+    [SerializeField] private GameObject enemyPrefab; 
+    [SerializeField] private int numberOfSpawn = 10; 
 
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {   temps = Time.time;      
         SubscribeToFlag();
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        numberOfEnemies = enemies.Length;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        temps = Time.time;
     }
 
 
     private void SetTheKing(GameObject player)
     {
         king = player;
+        CreateEnemies(); 
     }
 
     private void SubscribeToFlag()
@@ -45,6 +45,23 @@ public class Game : MonoBehaviour
         else
         {
             Debug.LogWarning("The gameObject \"Flag\" is not found in the current scene.");
+        }
+    }
+
+    private void CreateEnemies()
+    {
+        for (int i =0; i<numberOfSpawn; i++)
+        {
+            float x = Random.Range(5f,21*6f); 
+            x = king.transform.position.x + x;
+            float z = Random.Range(5f,21*6f);
+            z = king.transform.position.z + z;
+
+            GameObject nouvelEnemy = Instantiate(enemyPrefab); 
+            nouvelEnemy.transform.position = new Vector3(x,0f,z); 
+            EnemyController nouvelEnemyContr= nouvelEnemy.GetComponent<EnemyController>();
+            nouvelEnemyContr.TransitionToState(new KingChaseState(nouvelEnemyContr, king));
+            numberOfEnemies++; 
         }
     }
 
